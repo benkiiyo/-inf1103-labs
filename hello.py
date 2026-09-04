925ce35b6d9d2d6a85fb606000774893a3501d73
Namita <namianu0@gmail.com>
2026-09-04 17:14:31 +0800

    Activity 2 & 3

--- hello.py
+++ hello.py
@@ -1,4 +1,27 @@
+
+# activity 1
 print("===========================")
 print("Welcome here")
 print("My first post!")
 print("===========================")
+
+
+# activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+
+print("Username:", username)
+print("Bio:", bio)
+
+
+# activity 3
+followers = 100
+
+followers += 50  # Gained 50 new followers
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
