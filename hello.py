7c7f350545113aca8574e6f3af9c8f8cf803602b
Namita <namianu0@gmail.com>
2026-09-05 18:38:19 +0800

    Activity 5

--- hello.py
+++ hello.py
@@ -29,7 +29,7 @@ print("Day 3:", followers)  """
 
 # activity 4
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter your age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -37,3 +37,6 @@ print("======================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
